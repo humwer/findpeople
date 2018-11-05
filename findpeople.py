c8da12810704136712b0771da6a359b8b39ba156
humwer <dev.humwer@gmail.com>
2018-11-05 21:10:33 +0700

    raw find

--- findpeople.py
+++ findpeople.py
@@ -3,10 +3,7 @@ import vk_api
 login, password = 'your@email.com', 'yourpassword'
 
 vk_session = vk_api.VkApi(login, password)
-try:
-	vk_session.auth(token_only = True)
-except vk_api.AuthError as error_msg:
-	print(error_msg)
+vk_session.auth(token_only = True)
 
 vk = vk_session.get_api()
 
@@ -20,4 +17,18 @@ while i < num:
 	arr.append(buf[0]['users']['items'][i])
 	i+=1
 
+param = {'sex, city'}
+# for elem in arr:
+# 	man = vk.users.get(user_ids = elem, fields = param)
+# 	try:
+# 		print(man[0]['first_name'] + ' ' + man[0]['last_name'] + ' ' + str(man[0]['city']['id']) + ' ' + man[0]['city']['title'])
+# 	except BaseException:
+# 		continue
 
+for elem in arr:
+	man = vk.users.get(user_ids = elem, fields = param)
+	try:
+		if  str(man[0]['city']['id']) == '1':
+			print(man[0]['first_name'] + ' ' + man[0]['last_name'])
+	except BaseException:
+		continue
